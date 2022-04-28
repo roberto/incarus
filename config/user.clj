4d0c9486a2edfc6d8a264aa5de069ec8a79b0ce4
Roberto Soares <roberto.soares@nubank.com.br>
2022-04-28 18:19:26 -0300

    Add dependencies
    Co-authored-by: Filipe Andrade <filipe.andrade@nubank.com.br>

---
+++ config/user.clj
@@ -0,0 +1,3 @@
+;; File is required for project.clj :repl-options {:init user} to work
+(ns user
+  (:require [clojure.tools.namespace.repl :refer [refresh refresh-all]]))
--- project.clj
+++ project.clj
@@ -2,5 +2,11 @@
   :description "Redis/Carmine increment/decrement by with limit"
   :url "http://github.com/roberto/incarus"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns incarus.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.taoensso/carmine "3.1.0" :exclusions [org.clojure/clojure]]]
+  :profiles {:dev {:source-paths ["config" "resources"]
+                   :resource-paths ["resources"]
+                   :dependencies [[clj-test-containers "0.7.0"]
+                                  [org.clojure/tools.namespace "0.2.10"]]}}
+  :repl-options {:init-ns user}
+  :resource-paths ["resources"])
